af5d34667b53e5a96557c5b4e40419a609c79dc5
Nick Solomon <nsolomon@council.nyc.gov>
2019-01-09 12:26:35 -0500

    remove st_simplify

--- data-raw/make_council_dists.R
+++ data-raw/make_council_dists.R
@@ -5,12 +5,12 @@ library(purrr)
 
 set.seed(20190108)
 
-dists <- sf::st_read("https://data.cityofnewyork.us/api/geospatial/yusd-j4xi?method=export&format=GeoJSON") %>%
-  sf::st_simplify()
+dists <- sf::st_read("https://data.cityofnewyork.us/api/geospatial/yusd-j4xi?method=export&format=GeoJSON")
+
+names(st_geometry(dists)) <- NULL
 
 dists <- dists %>%
   mutate(lab_coord = map(geometry, ~poi(st_coordinates(.), precision = .000001)),
          lab_x = map_dbl(lab_coord, "x"),
          lab_y = map_dbl(lab_coord, "y"))
 usethis::use_data(dists, internal = TRUE, overwrite = TRUE)
-
