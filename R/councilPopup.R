fe530b55ce0545211c24e6c96e84165e50d2b826
Nick Solomon <nsolomon@council.nyc.gov>
2019-01-09 12:26:46 -0500

    add popup function

---
+++ R/councilPopup.R
@@ -0,0 +1,20 @@
+#' Basic Styling for Leaflet Popups
+#'
+#' This function wraps its input in a \code{<div>} and sets council fonts for
+#' body and headers.
+#'
+#' @param x The popup HTML
+#'
+#' @return A character vector
+#' @export
+#'
+#' @examples
+#'
+#' councilPopup("Test")
+councilPopup <- function(x) {
+
+  style <- "h1,h2,h3,h4,h5 {font-family: Georgia, serif;}
+font-family: 'Open Sans', sans-serif;"
+
+  paste0("<div>", "<style>", style, "</style>", x, "</div>")
+}
