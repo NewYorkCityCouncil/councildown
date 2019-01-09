6e93cdeb76011c9ec53e448ae766f2c555fd090a
Nick Solomon <nsolomon@council.nyc.gov>
2019-01-09 11:25:36 -0500

    Add styling for council districts

--- R/addCouncilStyle.R
+++ R/addCouncilStyle.R
@@ -1,6 +1,7 @@
 #' Style a leaflet map
 #'
 #' @param map A \code{leaflet} map
+#' @param add_dists Boolean. Add council districts?
 #'
 #' @return A \code{leaflet} map that in has City Council styles, including tiles,
 #'    council district outlines, and fonts
@@ -9,20 +10,29 @@
 #' @examples
 #' library(leaflet)
 #' leaflet() %>%
-#'   addCouncilStyle
+#'   addCouncilStyle()
 #'
-addCouncilStyle <- function(map) {
+addCouncilStyle <- function(map, add_dists = TRUE) {
 
-  map %>%
-    leaflet::addTiles(urlTemplate = "//cartodb-basemaps-{s}.global.ssl.fastly.net/light_nolabels/{z}/{x}/{y}.png") %>%
-    leaflet::addPolygons(data = dists, fill = FALSE, weight = .5, color = "black", opacity = .2) %>%
-    leaflet::addLabelOnlyMarkers(data = dists, lat = ~lab_y, lng = ~lab_x, label = ~coun_dist,
-                        labelOptions = leaflet::labelOptions(permanent = TRUE, noHide = TRUE,
-                                                    textOnly = TRUE,
-                                                    textsize = "13px",
-                                                    direction = "center",
-                                                    style = list(color = "#0004",
-                                                                 `font-family` = "'Open Sans', sans-serif",
-                                                                 `font-weight` = "bold"))) %>%
-    htmlwidgets::prependContent(htmltools::tags$style("@import url('https://fonts.googleapis.com/css?family=Open+Sans'); .leaflet-control {font-family: 'Open Sans', sans-serif;}"))
+  map <- map %>%
+    leaflet::addTiles(urlTemplate = "//cartodb-basemaps-{s}.global.ssl.fastly.net/light_nolabels/{z}/{x}/{y}.png",
+                      options = tileOptions(maxZoom = 12)) %>%
+    leaflet::addTiles(urlTemplate = "//cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png",
+                      options = tileOptions(minZoom = 13, maxZoom = 17)) %>%
+    htmlwidgets::prependContent(htmltools::tags$style("@import url('https://fonts.googleapis.com/css?family=Open+Sans:400,700'); .leaflet-control {font-family: 'Open Sans', sans-serif;}"))
+
+  if(add_dists) {
+    map <- map %>%
+      leaflet::addPolygons(data = dists, fill = FALSE, weight = 1, color = "#2F56A6", opacity = .5) %>%
+      leaflet::addLabelOnlyMarkers(data = dists, lat = ~lab_y, lng = ~lab_x, label = ~coun_dist,
+                                   labelOptions = leaflet::labelOptions(permanent = TRUE, noHide = TRUE,
+                                                                        textOnly = TRUE,
+                                                                        textsize = 12,
+                                                                        direction = "center",
+                                                                        style = list(color = "#23417D",
+                                                                                     `font-family` = "'Open Sans', sans-serif",
+                                                                                     `font-weight` = "bold")))
+  }
+
+  return(map)
 }
