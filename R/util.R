a779d3593463dcf9245fbb0d69181ec56f219cd7
Nick Solomon <nsolomon@council.nyc.gov>
2019-02-26 14:38:43 -0500

    embed fonts in pdfs

--- R/util.R
+++ R/util.R
@@ -19,3 +19,35 @@ pretty_date <- function(x = NULL) {
   packageStartupMessage("Using NYCC default colors and theme in `ggplot2` calls. To use `ggplot2` defaults call them with `::`.")
   ggplot2::theme_set(councildown::theme_nycc())
 }
+
+
+# Taken from yihui/knitr/R/util.R
+
+`%n%` = getFromNamespace("%n%", "knitr")
+
+all_figs = getFromNamespace("all_figs", "knitr")
+
+in_base_dir = getFromNamespace("in_base_dir", "knitr")
+
+
+#' Embed fonts in PDF images
+#'
+#' This knitr chunk hook embeds fonts in PDF images so that the appear correctly
+#' on all computers.
+#'
+#' @param before Is hook being run before chunk
+#' @param options Chunk options
+#' @param envir Environment
+#'
+#' @export
+#'
+#' @importFrom utils getFromNamespace
+
+hook_pdfembed <- function(before, options, envir) {
+  # crops plots after a chunk is evaluated and plot files produced
+  ext = options$fig.ext
+  if (options$dev == 'tikz' && options$external) ext = 'pdf'
+  if (before || (fig.num <- options$fig.num %n% 0L) == 0L) return()
+  paths = all_figs(options, ext, fig.num)
+  in_base_dir(for (f in paths) extrafont::embed_fonts(f))
+}
