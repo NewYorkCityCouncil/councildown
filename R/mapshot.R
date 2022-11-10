52708892d8628bda3b3debbc37a84c8ab932f0f2
nmontalbano <nmontalbanocouncil@gmail.com>
2022-11-10 15:28:47 -0500

    add mapshot example and get functions from mapview

--- R/mapshot.R
+++ R/mapshot.R
@@ -8,6 +8,14 @@
 #' @export
 #'
 #' @examples
+#' \dontrun{
+#' library(leaflet)
+#'
+#' m <- leaflet() %>%
+#'  addTiles()
+#' mapshot(m, file = "test.png")
+#' file.remove("test.pdf")
+#' }
 #'
 #' @import mapview
 #' @importFrom tools file_path_sans_ext
@@ -28,18 +36,18 @@ mapshot <- function (x, url = NULL, file = NULL, remove_controls = c("zoomContro
     if (avl_file)
       if (file_ext(file) == "png" | file_ext(file) == "jpeg") {
         file = paste0(file_path_sans_ext(file), ".pdf")
-        warning("councildown overrides .png and .jpeg extensions with .pdf to improve image quality")
+        warning("councildown overrides .png and .jpeg extensions with .pdf to improve image quality\nuse mapview::mapshot if you want the default function")
       }
       file = normalizePath(file, mustWork = FALSE)
     if (inherits(x, "mapview")) {
       x = mapview2leaflet(x)
     }
-    if (!inherits(x, "leaflet") | is_literally_false(remove_controls)) {
+    if (!inherits(x, "leaflet") | mapview:::is_literally_false(remove_controls)) {
       remove_controls = NULL
     }
     if (!avl_url) {
       url = tempfile(fileext = ".html")
-      x = removeMapJunk(x, remove_controls)
+      x = mapview:::removeMapJunk(x, remove_controls)
     }
     args = list(url = url, file = file, ...)
     sw_ls = args
@@ -60,7 +68,7 @@ mapshot <- function (x, url = NULL, file = NULL, remove_controls = c("zoomContro
                        "_files")
       sw_ls = utils::modifyList(sw_ls, list(file = tmp_url))
       args$url = tmp_url
-      x = removeMapJunk(x, remove_controls)
+      x = mapview:::removeMapJunk(x, remove_controls)
       do.call(htmlwidgets::saveWidget, append(list(x), sw_ls[sw_args]))
       do.call(webshot::webshot, args[ws_args])
       return(invisible())
